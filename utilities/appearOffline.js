8e53b7f7b33168423fbac90fb1f01039e4600d4c
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-06-03 06:15:07 -0400

    Moved stuff

--- utilities/appearOffline.js
+++ utilities/appearOffline.js
@@ -1,3 +1,5 @@
+alert('Script coded by Zastix, Betastar tester\nFor more scripts, visit\nhttps://villainsrule2000.github.io/Betastar/')
+
 alert('WARNING: you will not be able to trade with this script enabled')
 socket.disconnect()
 if (location.pathname === '/stats/' || location.pathname === '/stats') {
