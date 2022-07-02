fcb0fee0ab619921232e977a206200f620474e69
VillainsRule from Villain Devs!! <101288516+VillainsRule4000@users.noreply.github.com>
2022-07-01 23:26:24 -0400

    Hacks long updated

--- Hacks/appearOffline.js
+++
@@ -1,7 +0,0 @@
-alert('Script coded by Zastix, Betastar tester\nFor more scripts, visit\nhttps://villainsrule2000.github.io/Betastar/')
-
-alert('WARNING: you will not be able to trade with this script enabled')
-socket.disconnect()
-if (location.pathname === '/stats/' || location.pathname === '/stats') {
-  document.getElementById('#userElement').style.filter = 'drop-shadow(0px 0px 100px red)'
-}
